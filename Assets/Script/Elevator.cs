93c037e1e0240bd97b60f0499641022f82a4df20
loleksatyr <czarnotabartek@gmail.com>
2019-11-29 14:10:12 +0100

    IA start

---
+++ Assets/Script/Elevator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed1 = 1f;
+    public static bool active;
+    bool counter = false;
+    bool nadol;
+    
+    void Start()
+    {
+        var spherec = GetComponent<Renderer>();
+        spherec.material.SetColor("_Color", Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (counter)
+            {
+                StartCoroutine(Example());
+                counter = false;
+                Debug.Log("gora"); 
+                
+        }
+            if(active)
+            transform.Translate(Vector3.up * Time.deltaTime * speed1);
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("dzialaj");
+        if (collision.gameObject.name == "Player")
+        {
+            counter = true;
+        }
+        if (collision.gameObject.name == "EndPoint")
+        {
+            active = false;
+            Debug.Log("stop");
+        }
+
+
+
+    }
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(2);
+        active = true;
+        print(Time.time);
+    }
+}
---
+++ Assets/Script/MovementScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    float speed = 5f;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up* speed, ForceMode.Impulse );
+        }
+        */
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = transform.right * speed;
+            // rb.velocity = transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+
+        {
+            rb.velocity = -transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = -transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down),1))
+            rb.AddForce(transform.up * speed, ForceMode.Impulse);
+            
+        }
+
+    }
+     void OnCollision(Collision collision)
+    {
+        if (collision.gameObject.name == "EndPoint")
+        {
+            Elevator.active = false;
+        }
+    }
+}
